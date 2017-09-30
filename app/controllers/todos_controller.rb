73a72eb3a8e545581fa569c3eee36165501b9c8b
keianna <kei2001kei@gmail.com>
2017-09-30 18:00:37 +0000

    make smart

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,18 @@ class TodosController< ApplicationController
     def index 
     end
     
-    def show
-    end
+    def show 
+        @todo =
+        {
+            id: params[:id],
+            
+            name: 'MOCHA',
+            
+            description:'Mocha needs to be fed around 5pm',
+            
+            duration:10
+        }
+            
+        
+    end 
 end
