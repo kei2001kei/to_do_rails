5cd992ad820221771ad5cccd0b3b495946ea8b2c
keianna <kei2001kei@gmail.com>
2017-10-01 16:45:26 +0000

    background

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,33 +3,13 @@
             @Code_word= "#{params[:code]} #{params[:word]}"
         end
     
-        def show 
-        id =params[:id]
-        
-        if id=='1'
-            @todo = {
-            id:id,
-            name:'Do homework',
-            duration:60
-            }
-            elsif id=='2'
-            
-        @todo =
-        {
-            id: params[:id],
-            
-            name: 'MOCHA',
-            
-            description:'Mocha needs to be fed around 5pm',
-            
-            duration:10
-        }
-    else
-        @todo={} 
+        def show
+            @todo=Todo.find(params[:id])
         
         end 
         
-        end
+        
+        
         def create 
             todo= Todo.create(
                 name: params[:name],
@@ -39,4 +19,5 @@
             )
             redirect_to "/todos/#{todo.id}"
         end
+        
         end 
