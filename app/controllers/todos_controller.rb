90587de8df89d70c525b615da9b3936ef76a27fc
keianna <kei2001kei@gmail.com>
2017-10-01 16:17:56 +0000

    submit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -26,7 +26,17 @@
         }
     else
         @todo={} 
-        end 
-        end
-        end
         
+        end 
+        
+        end
+        def create 
+            todo= Todo.create(
+                name: params[:name],
+            description: params[:description],
+            duration: params[:duration],
+            complete: params[:complete]
+            )
+            redirect_to "/todos/#{todo.id}"
+        end
+        end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  root 'todos#index'
  get 'todos' => 'todos#index'
  get 'todos/:id' => 'todos#show'
+post 'todos' => 'todos#create' 
  
 end
